bdaa7a5ba4b324a2049a593a54b3605a80796c71
andres fragoso <afragoso@hotmail.com>
2025-06-30 15:27:58 -0700

    con fondo

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,37 @@
 import Link from 'next/link';
+import Head from 'next/head';
 
 export default function Home() {
   return (
-    <div>
-      <h1>Bienvenido a Halcón en Camino</h1>
-      <Link href="/login">Iniciar sesión</Link>
-    </div>
+    <>
+      <Head>
+        <style>{`
+          body {
+            background-image: url('/background.jpg');
+            background-size: cover;
+            background-position: center;
+            margin: 0;
+            font-family: sans-serif;
+            color: white;
+            text-shadow: 1px 1px 4px black;
+          }
+          .container {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            height: 100vh;
+            background-color: rgba(0, 0, 0, 0.5);
+          }
+          h1 {
+            font-size: 3rem;
+          }
+        `}</style>
+      </Head>
+      <div className="container">
+        <h1>Bienvenido a Halcón en Camino</h1>
+        <Link href="/login">Iniciar sesión</Link>
+      </div>
+    </>
   );
 }
